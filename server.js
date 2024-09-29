f472ec3800a957c9d4e0f0cc3b485502e03f6878
amaykorade <amaykorade5@gmail.com>
2024-09-29 21:46:19 +0530

    added

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import app from './app.js'
+// import app from './app.js'
 
-app.listen(3000, () => {
+// app.listen(3000, () => {
 
-    console.log('Server is running on port 3000');
-});
+//     console.log('Server is running on port 3000');
+// });
