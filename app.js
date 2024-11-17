9a3855104cf220497da0a048bb466f190525fc0b
amaykorade <amaykorade5@gmail.com>
2024-11-17 16:39:53 +0530

    Updated some things

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send('Hello World');
 })
 
-app.listen(3000, () => {
-
-    console.log('Server is running on port 3000');
-});
+module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// import app from './app.js'
+const app = require('./app');
 
-// app.listen(3000, () => {
+app.listen(3000, () => {
 
-//     console.log('Server is running on port 3000');
-// });
+    console.log('Server is running on port 3000');
+});
